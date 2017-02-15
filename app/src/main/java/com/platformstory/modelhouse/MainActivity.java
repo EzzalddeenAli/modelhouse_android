ea273e1fa85d5c6393144d51baca113ea340f62b
DESKTOP-ALQPFF6\Midas Yoon <goback2u@naver.com>
2017-02-15 14:31:47 +0900

    다음 지도 API 연동

--- app/src/main/java/com/platformstory/modelhouse/MainActivity.java
+++ app/src/main/java/com/platformstory/modelhouse/MainActivity.java
@@ -2,6 +2,11 @@ package com.platformstory.modelhouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import net.daum.mf.map.api.MapView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapView mapView = new MapView(this);
+        mapView.setDaumMapApiKey("c28ce2a178185b5577a869519a3ac74b");
+
+        RelativeLayout mapViewContainer = (RelativeLayout) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
     }
 }
