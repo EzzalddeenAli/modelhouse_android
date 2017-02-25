b50e6eaad7f287b2e725be30fe6357d2f69cea4f
MidasYoon <goback2u@naver.com>
2017-02-25 14:43:23 +0900

    구글 지도 띄우기 작

--- Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
+++ Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
@@ -3,11 +3,12 @@ package com.platformstory.modelhouse;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 
     public void onCreate(Bundle savedInstanceState) {
