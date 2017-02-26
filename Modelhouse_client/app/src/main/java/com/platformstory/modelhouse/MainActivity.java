505d7d3d09c8b963a455202e534f6a37a17cf6a9
MidasYoon <goback2u@naver.com>
2017-02-27 00:11:04 +0900

    클러스터 생성 작업 및 좌표 이동시 클러스터 생성 작업 1차 완료

--- Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
+++ Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends FragmentActivity {
         longitude = Double.parseDouble(settings.getString("longitude", "126.9786567859"));
         zoom = settings.getInt("zoom", 14);
 
+        // 지도 객체를 생성하고 지도를 표시한 후 좌표, 줌 값을 토대로 중심을 이동
         gMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), zoom));
@@ -64,19 +65,6 @@ public class MainActivity extends FragmentActivity {
         // 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
         EstateSearchMapThread thread = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
         thread.start();
-
-        gMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-            @Override
-            public void onCameraChange(CameraPosition cameraPosition) {
-                latitude = cameraPosition.target.latitude;
-                longitude = cameraPosition.target.longitude;
-
-                // 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
-                EstateSearchMapThread thread2 = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
-                thread2.start();
-            }
-        });
-
     }
 
     class EstateSearchMapThread extends Thread {
@@ -104,19 +92,33 @@ public class MainActivity extends FragmentActivity {
                 ja = new JSONArray(JSON);
 
                 //위도, 경도 값들을 토대로 지도에 클러스트를 만들고, 리스트 버튼 클릭 시 제이슨 배열을 인텐트로 넘겨주는 로직을 이곳에 작성
-                mClusterManager = new ClusterManager<MyItem>(MainActivity.this, gMap);
+                mClusterManager = new ClusterManager<MyItem>(MainActivity.this, gMap){
+                    // 지도의 중심이 바뀌면 바뀐 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
+                    @Override
+                    public void onCameraChange(CameraPosition cameraPosition) {
+                        super.onCameraChange(cameraPosition);
 
-//                gMap.setOnCameraChangeListener(mClusterManager);
-//                gMap.setOnMarkerClickListener(mClusterManager);
+                        latitude = cameraPosition.target.latitude;
+                        longitude = cameraPosition.target.longitude;
+
+                        EstateSearchMapThread thread2 = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
+                        thread2.start();
+                    }
+                };
+
+                gMap.setOnCameraChangeListener(mClusterManager);
+                gMap.setOnMarkerClickListener(mClusterManager);
 
                 addItems(ja);
             }catch (JSONException e){
 
             }
 
+            // 텍스트뷰에 검색 결과의 개수를 표시한다.
             TextView result_count = (TextView)findViewById(R.id.result_count);
             result_count.setText(ja.length() + "개");
 
+            // 이 버튼을 클릭하면 해당 지역(지도 중심 반경)의 검색 결과 개수만큼 리스트로 보여주는 페이지로 이동
             Button button = (Button)findViewById(R.id.button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -129,13 +131,28 @@ public class MainActivity extends FragmentActivity {
         }
     };
 
+    // 클러스터를 띄우는데 사용되는 위도,경도 값을 저장하기 위한 객체 정의
+    public class MyItem implements ClusterItem {
+        private final LatLng mPosition;
+
+        public MyItem(double lat, double lng) {
+            mPosition = new LatLng(lat, lng);
+        }
+
+        @Override
+        public LatLng getPosition() {
+            return mPosition;
+        }
+    }
+
+    // 서버에서 받아온 제이슨 객체로부터 위도, 경도 값을 가져와서 클러스터를 만들기 위한 작업 실시
     private void addItems(JSONArray ja) {
         try {
             for (int i = 0; i < ja.length(); i++) {
                 JSONObject estateObj = ja.getJSONObject(i);
 
                 MyItem offsetItem = new MyItem(estateObj.getDouble("latitude"), estateObj.getDouble("longtitude"));
-                Log.i("modelhouse", estateObj.getDouble("latitude") + ", " + estateObj.getDouble("longtitude"));
+                //Log.i("modelhouse", estateObj.getDouble("latitude") + ", " + estateObj.getDouble("longtitude"));
 
                 mClusterManager.addItem(offsetItem);
             }
@@ -157,18 +174,6 @@ public class MainActivity extends FragmentActivity {
         editor.commit();
     }
 
-    public class MyItem implements ClusterItem {
-        private final LatLng mPosition;
-
-        public MyItem(double lat, double lng) {
-            mPosition = new LatLng(lat, lng);
-        }
-
-        @Override
-        public LatLng getPosition() {
-            return mPosition;
-        }
-    }
 
 }
 
