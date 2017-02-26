b109e97332960ef54605db56197b09c49ac01bc5
MidasYoon <goback2u@naver.com>
2017-02-26 23:37:17 +0900

    지도 검색 디자인 작업, 지도에서 리스트로 넘어가는 방식 변경

--- Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/EstateSearchListActivity.java
+++ Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/EstateSearchListActivity.java
@@ -24,80 +24,47 @@ public class EstateSearchListActivity  extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.estate_search_list);
 
-//        Intent intent = getIntent();
-//        String JSON = intent.getStringExtra("JSON");
+        Intent intent = getIntent();
+        String JSON = intent.getStringExtra("JSON");
 
-        mProgress = ProgressDialog.show(EstateSearchListActivity.this, "Wait", "Downloading...");
+        estates = new ArrayList<Estate>();
 
-        DownThread  thread = new DownThread("http://52.79.106.71/estates?latitude=37.5668260055&longtitude=126.9786567859");
-        thread.start();
+        try {
+            JSONArray ja = new JSONArray(JSON);
+
+            //Log.i("modelhouse", ja.length()+"");
+
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject estateObj = ja.getJSONObject(i);
+                Estate estate = new Estate(estateObj.getInt("id"), estateObj.getString("type"), estateObj.getString("photo"), estateObj.getString("price_type"),
+                        estateObj.getString("price"),
+                        estateObj.getString("extent"), estateObj.getString("category"), estateObj.getString("usearea"), estateObj.getString("facility"),
+                        estateObj.getString("addr1"), estateObj.getString("info"), estateObj.getDouble("latitude"), estateObj.getDouble("longtitude"));
+                estates.add(estate);
+            }
+        }catch (JSONException e){
+
+        }
+
+        EstateList estateList = new EstateList(EstateSearchListActivity.this, R.layout.estate_list, estates);
+
+        ListView MyList = (ListView)findViewById(R.id.list);
+        MyList.setAdapter(estateList);
+
+        MyList.setOnItemClickListener(estateDetail);
     }
 
-    class DownThread extends Thread {
-        String mAddr;
+    AdapterView.OnItemClickListener estateDetail = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent intent = new Intent(EstateSearchListActivity.this, EstateDetailActivity.class);
+            intent.putExtra("estate_id", estates.get(position).id+"");
+            intent.putExtra("latitude", estates.get(position).latitude);
+            intent.putExtra("longtitude", estates.get(position).longtitude);
 
-        DownThread(String addr) {
-            mAddr = addr;
+            startActivity(intent);
+
+            //Toast.makeText(MainActivity.this, estates.get(position).id+"", Toast.LENGTH_LONG).show();
         }
-
-        public void run() {
-            String Json = Network.DownloadHtml(mAddr);
-
-            estates = new ArrayList<Estate>();
-
-            //Log.i("modelhouse", Json);
-
-            try{
-                JSONArray ja = new JSONArray(Json);
-
-                //Log.i("modelhouse", ja.length()+"");
-
-                for(int i=0; i<ja.length(); i++){
-                    JSONObject estateObj = ja.getJSONObject(i);
-                    Estate estate = new Estate(estateObj.getInt("id"), estateObj.getString("type"), estateObj.getString("photo"), estateObj.getString("price_type"),
-                            estateObj.getString("price"),
-                            estateObj.getString("extent"), estateObj.getString("category"), estateObj.getString("usearea"), estateObj.getString("facility"),
-                            estateObj.getString("addr1"), estateObj.getString("info"), estateObj.getDouble("latitude"), estateObj.getDouble("longtitude"));
-                    estates.add(estate);
-                }
-
-                //Log.i("modelhouse", estates.size()+"");
-            }catch (JSONException e){
-
-            }
-
-            Message message = mAfterDown.obtainMessage();
-            message.obj = estates;
-            mAfterDown.sendMessage(message);
-        }
-    }
-
-    Handler mAfterDown = new Handler() {
-        public void handleMessage(Message msg) {
-            mProgress.dismiss();
-
-            estates = (ArrayList<Estate>)msg.obj;
-
-            EstateList estateList = new EstateList(EstateSearchListActivity.this, R.layout.estate_list, estates);
-
-            ListView MyList = (ListView)findViewById(R.id.list);
-            MyList.setAdapter(estateList);
-
-            MyList.setOnItemClickListener(estateDetail);
-        }
-
-        AdapterView.OnItemClickListener estateDetail = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(EstateSearchListActivity.this, EstateDetailActivity.class);
-                intent.putExtra("estate_id", estates.get(position).id+"");
-                intent.putExtra("latitude", estates.get(position).latitude);
-                intent.putExtra("longtitude", estates.get(position).longtitude);
-
-                startActivity(intent);
-
-                //Toast.makeText(MainActivity.this, estates.get(position).id+"", Toast.LENGTH_LONG).show();
-            }
-        };
     };
 }
--- Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
+++ Modelhouse_client/app/src/main/java/com/platformstory/modelhouse/MainActivity.java
@@ -23,6 +23,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+import com.google.maps.android.clustering.ClusterManager;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -37,7 +39,10 @@ public class MainActivity extends FragmentActivity {
     double longitude;
     int zoom;
 
+    ClusterManager<MyItem> mClusterManager;
+
     String JSON;
+    JSONArray ja;
     ProgressDialog mProgress;
 
     public void onCreate(Bundle savedInstanceState) {
@@ -52,22 +57,13 @@ public class MainActivity extends FragmentActivity {
         longitude = Double.parseDouble(settings.getString("longitude", "126.9786567859"));
         zoom = settings.getInt("zoom", 14);
 
-        // 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
-        EstateSearchMapThread thread = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
-        thread.start();
-
         gMap = ((SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), zoom));
 
-        Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, EstateSearchListActivity.class);
-                startActivity(intent);
-            }
-        });
+        // 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
+        EstateSearchMapThread thread = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
+        thread.start();
 
         gMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
             @Override
@@ -76,8 +72,8 @@ public class MainActivity extends FragmentActivity {
                 longitude = cameraPosition.target.longitude;
 
                 // 지도의 좌표, 줌 값을 토대로 지도 중심 반경 내의 매물을 검색하여 클러스터로 띄움
-                EstateSearchMapThread thread = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
-                thread.start();
+                EstateSearchMapThread thread2 = new EstateSearchMapThread("http://52.79.106.71/estates?latitude="+latitude+"&longtitude="+longitude+"&zoom="+zoom);
+                thread2.start();
             }
         });
 
@@ -105,31 +101,48 @@ public class MainActivity extends FragmentActivity {
 
             try{
                 JSON = (String)msg.obj;
-                JSONArray ja = new JSONArray(JSON);
+                ja = new JSONArray(JSON);
 
-                for(int i=0; i<ja.length(); i++) {
-                    JSONObject estateObj = ja.getJSONObject(i);
-
-                    Log.i("modelhouse", estateObj.getDouble("latitude")+", "+estateObj.getDouble("longtitude"));
-                    //estateObj.getDouble("latitude"); estateObj.getDouble("longtitude");
-                }
                 //위도, 경도 값들을 토대로 지도에 클러스트를 만들고, 리스트 버튼 클릭 시 제이슨 배열을 인텐트로 넘겨주는 로직을 이곳에 작성
+                mClusterManager = new ClusterManager<MyItem>(MainActivity.this, gMap);
 
+//                gMap.setOnCameraChangeListener(mClusterManager);
+//                gMap.setOnMarkerClickListener(mClusterManager);
 
-//                Button button = (Button)findViewById(R.id.button);
-//                button.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        Intent intent = new Intent(MainActivity.this, EstateSearchListActivity.class);
-//                        intent.putExtra("JSON", JSON);
-//                        startActivity(intent);
-//                    }
-//                });
+                addItems(ja);
             }catch (JSONException e){
 
             }
+
+            TextView result_count = (TextView)findViewById(R.id.result_count);
+            result_count.setText(ja.length() + "개");
+
+            Button button = (Button)findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, EstateSearchListActivity.class);
+                    intent.putExtra("JSON", JSON);
+                    startActivity(intent);
+                }
+            });
         }
     };
+
+    private void addItems(JSONArray ja) {
+        try {
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject estateObj = ja.getJSONObject(i);
+
+                MyItem offsetItem = new MyItem(estateObj.getDouble("latitude"), estateObj.getDouble("longtitude"));
+                Log.i("modelhouse", estateObj.getDouble("latitude") + ", " + estateObj.getDouble("longtitude"));
+
+                mClusterManager.addItem(offsetItem);
+            }
+        }catch (JSONException e){
+
+        }
+    }
 
     //앱이 종료되면 현재 지도의 좌표, 줌 값을 저장
     @Override
@@ -143,4 +156,21 @@ public class MainActivity extends FragmentActivity {
         editor.putInt("zoom", zoom);
         editor.commit();
     }
+
+    public class MyItem implements ClusterItem {
+        private final LatLng mPosition;
+
+        public MyItem(double lat, double lng) {
+            mPosition = new LatLng(lat, lng);
+        }
+
+        @Override
+        public LatLng getPosition() {
+            return mPosition;
+        }
+    }
+
 }
+
+
+
